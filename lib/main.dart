7a480b58561718e76ca0bfec792f24e9934f805e
AmanSaini <aman.saini0200@gmail.com>
2022-01-25 11:37:00 +0530

    vinay_first_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,61 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-void main(){
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyForm(),
-    ),
-  );
+void main()
+{
+  runApp(MyForm());
 }
 
-class MyForm extends StatefulWidget{
-  const MyForm({Key? key}) : super(key: key);
+class MyForm extends StatelessWidget {
 
-  @override
-  State<StatefulWidget> createState() {
-    return MyFormState();
-  }
-}
+  final _formkey = GlobalKey<FormBuilderState>();
 
-class MyFormState extends State<MyForm>{
-
-  final _myFormkey = GlobalKey<FormState>();
-  late DateTime _dateTime=DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar : AppBar(
-        title : const Text("My Form"),
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 25.0,horizontal: 25.0),
-        child: Form(
-          key: _myFormkey,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Name",
-                    hintText: "Enter Your name",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(3.0),
+    return MaterialApp(
+      title: "Form Builder",
+      home: Scaffold(
+        appBar: AppBar(title: Text("MY FORM")),
+        body: Center
+          (child: Container(
+          child: Form(
+            child: Column(children: <Widget>[
+              Container(
+                  child: TextFormField(
+                      validator: (msg)
+                      {
+                        if(msg!.isEmpty)
+                        {
+                          return "please Enter name";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Name",
+                        hintText: "Enter Your Name",
+                      )
+                  )
+              ),
+              Container(
+                  child:
+                  FlatButton(
+                    onPressed: () => debugPrint("pressed"),
+                    child: const Text(
+                        "Next",
+                        style: TextStyle(fontSize: 17,
+                            backgroundColor: Colors.green,
+                            color: Colors.white),
                     ),
                   ),
-                  validator: (msg){
-                    if(msg!.isEmpty){
-                      return "Required";
-                    }
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  onSaved: (val) {
-                    _dateTime = DateTime.now();
-                  },
-                  keyboardType: TextInputType.datetime,
-                  decoration: InputDecoration(
-                    labelText: "Date",
-                    hintText: "Enter Your DOB",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(3.0),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter a date for your task";
-                    }
-                    return null;
-                  },
-                ),
-              ),
-                    /*
-                    showDatePicker(
-                        context: context,
-                        initialDate: _dateTime == null ? DateTime.now() : _dateTime,
-                        firstDate: DateTime(2001),
-                        lastDate: DateTime(2021)
-                    ).then((date) {
-                      setState(() {
-                        _dateTime = date;
-                      });
-                    });
-                    */
-              TextButton(
-                child: const Text("Submit"),
-                onPressed: () {
-                  if (_myFormkey.currentState!.validate()) {
-                    _myFormkey.currentState!.save();
-                  }
-                },
               ),
             ],
+            ),
           ),
         ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.done),
-        onPressed: () {
-          _myFormkey.currentState!.validate();
-        },
-    ),
     );
   }
 }
