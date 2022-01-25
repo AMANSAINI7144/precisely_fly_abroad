6280a0fcdf13767d6406f6ca9979b04cc2d3128c
AmanSaini <aman.saini0200@gmail.com>
2022-01-25 13:53:05 +0530

    radio and checkbox added

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,9 @@ class MyForm extends StatefulWidget{
 class MyFormState extends State<MyForm>{
 
   final _myFormkey = GlobalKey<FormState>();
-  late DateTime _dateTime=DateTime.now();
+  late DateTime _dateTime = DateTime.now();
+  int _value = 1;
+  bool _checkBoxValue = false;
 
   @override
   Widget build(BuildContext context) {
@@ -29,81 +31,184 @@ class MyFormState extends State<MyForm>{
       appBar : AppBar(
         title : const Text("My Form"),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 25.0,horizontal: 25.0),
-        child: Form(
-          key: _myFormkey,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Name",
-                    hintText: "Enter Your name",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(3.0),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 25.0,horizontal: 25.0),
+          child: Form(
+            key: _myFormkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Name",
+                      hintText: "Enter Your name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(3.0),
+                      ),
                     ),
+                    validator: (msg){
+                      if(msg!.isEmpty){
+                        return "Required";
+                      }
+                    },
                   ),
-                  validator: (msg){
-                    if(msg!.isEmpty){
-                      return "Required";
-                    }
-                  },
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  onSaved: (val) {
-                    _dateTime = DateTime.now();
-                  },
-                  keyboardType: TextInputType.datetime,
-                  decoration: InputDecoration(
-                    labelText: "Date",
-                    hintText: "Enter Your DOB",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(3.0),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    onSaved: (val) {
+                      _dateTime = DateTime.now();
+                    },
+                    keyboardType: TextInputType.datetime,
+                    decoration: InputDecoration(
+                      labelText: "Date",
+                      hintText: "Enter Your DOB",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(3.0),
+                      ),
                     ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter your Date Of Birth";
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter a date for your task";
-                    }
-                    return null;
-                  },
                 ),
-              ),
-                    /*
-                    showDatePicker(
-                        context: context,
-                        initialDate: _dateTime == null ? DateTime.now() : _dateTime,
-                        firstDate: DateTime(2001),
-                        lastDate: DateTime(2021)
-                    ).then((date) {
-                      setState(() {
-                        _dateTime = date;
+                /*
+                      showDatePicker(
+                          context: context,
+                          initialDate: _dateTime == null ? DateTime.now() : _dateTime,
+                          firstDate: DateTime(2001),
+                          lastDate: DateTime(2021)
+                      ).then((date) {
+                        setState(() {
+                          _dateTime = date;
+                        });
                       });
-                    });
-                    */
-              TextButton(
-                child: const Text("Submit"),
-                onPressed: () {
-                  if (_myFormkey.currentState!.validate()) {
-                    _myFormkey.currentState!.save();
-                  }
-                },
-              ),
-            ],
+                */
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Select Your Gender',
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Radio(
+                          value: 1,
+                          groupValue: _value,
+                          onChanged: (value){
+                            setState(() {
+                              _value = value as int;
+                            });
+                          },
+                        ),
+                        const Text("Male"),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Radio(
+                          value: 2,
+                          groupValue: _value,
+                          onChanged: (value){
+                            setState(() {
+                              _value = value as int;
+                            });
+                          },
+                        ),
+                        const Text("Female"),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Radio(
+                          value: 3,
+                          groupValue: _value,
+                          onChanged: (value){
+                            setState(() {
+                              _value = value as int;
+                            });
+                          },
+                        ),
+                        const Text("Other"),
+                      ],
+                    ),
+                    ],
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: _checkBoxValue,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _checkBoxValue = value as bool;
+                        });
+                      },
+                    ),
+                    const Text(
+                        "Please accept our Terms and conditions"
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      TextButton(
+                        child: const Text("Prev"),
+                        style: TextButton.styleFrom(
+                          primary: Colors.black,
+                          backgroundColor: Colors.blueGrey,
+                          onSurface: Colors.grey,
+                          side: const BorderSide(
+                            width: 1,
+                            color: Colors.black12,
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_myFormkey.currentState!.validate()) {
+                            _myFormkey.currentState!.save();
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        width: 3.0,
+                      ),
+                      TextButton(
+                        child: const Text("Next"),
+                        style: TextButton.styleFrom(
+                          primary: Colors.black,
+                          backgroundColor: Colors.yellow,
+                          onSurface: Colors.grey,
+                          side: const BorderSide(
+                            width: 1,
+                            color: Colors.black12,
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_myFormkey.currentState!.validate()) {
+                            _myFormkey.currentState!.save();
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.done),
-        onPressed: () {
-          _myFormkey.currentState!.validate();
-        },
-    ),
     );
   }
 }
